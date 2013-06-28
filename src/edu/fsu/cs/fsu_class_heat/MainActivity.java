fa730e9f2ac99d7642554eb641f05224fe638884
Matt Lambo <mattlambo3@gmail.com>
2013-06-28 11:05:47 -0400

    Signed-off-by: Matt Lambo <mattlambo3@gmail.com>

--- src/edu/fsu/cs/fsu_class_heat/MainActivity.java
+++ src/edu/fsu/cs/fsu_class_heat/MainActivity.java
@@ -1,15 +1,26 @@
 package edu.fsu.cs.fsu_class_heat;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 
+
+public class MainActivity extends FragmentActivity {
+
+	GoogleMap map;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+				
+        map = ( (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapView)).getMap();
+        
+
 	}
 
 	@Override
